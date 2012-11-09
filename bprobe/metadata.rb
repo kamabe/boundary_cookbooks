5a224e288799cff395bbd22dcb731db959919528
Kayla Townsend <kayla.berry@americanroamer.com>
2012-11-09 13:12:23 -0600

    Change boundary probe version to 0.1.0

--- bprobe/metadata.rb
+++ bprobe/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "ops@boundary.com"
 license          "Apache 2.0"
 description      "Installs/Configures bprobe"
 long_description "Installs/Configures bprobe"
-version          "0.1"
+version          "0.1.0"
 
 %w{ ubuntu centos }.each do |os|
   supports os
